ccf277a6c8184b32f96c88f1521340bcf5e2ee2d
JedwardMook <jedwardmook@yahoo.com>
2023-11-29 18:55:11 -0600

    Changes links to an array of objects and creates p tag for each. Adds drawing for the switch back and forth, adds all projects for now.

--- index.js
+++ index.js
@@ -10,13 +10,16 @@ document.addEventListener("DOMContentLoaded", () => {
     face.addEventListener('click', () => meOrNotToggle())
     let meOrNot = false
 
+    // <h4 id="hero_project_key">Link:</h4><a href="${links}" class="hero_project_value">${links}</a>
+
     function renderHeroProject(project){
         const { projectName, links, description, technologysUsed, image} = project
         const heroContainer = document.getElementById("hero_project_container")
         heroProject.innerHTML =`
             <div id="hero_project_info">
               <h4 id="hero_project_key">Project Name:</h4><p class="hero_project_value"> ${projectName}<p>
-              <h4 id="hero_project_key">Link:</h4><a href="${links}" class="hero_project_value">${links}</a>
+              <h4 id="hero_project_key_links">Links:</h4>
+                <div id="links"></div>
               <h4 id="hero_project_key_desc">Description:</h4><p class="hero_project_value"> ${description}<p>
               <h4 id="hero_project_key">Technologies Used:</h4><p class="hero_project_value"> ${technologysUsed}<p>
             </div>
@@ -24,6 +27,19 @@ document.addEventListener("DOMContentLoaded", () => {
                 <img src=${image} alt="${projectName}" id="hero_project_image"/>
             </div>`;
         heroContainer.append(heroProject);
+        const linksContainer = document.getElementById('links')
+        createList(links)
+        function createList(arr){
+            arr.forEach(obj => {
+                const link = document.createElement('p');
+                link.id = "project_link"
+                for (const [key, value] of Object.entries(obj)){
+                    link.innerHTML = `<p>${key}: <a href=${value}>${value}</a></p>`
+                }
+                linksContainer.append(link)
+            })   
+        }
+        
     } 
     
     renderHeroProject(initial_hero_project)
@@ -59,19 +75,16 @@ document.addEventListener("DOMContentLoaded", () => {
             projectsEnd = projectsEnd - 1
             projects.slice(projectsStart,projectsEnd).forEach(renderProject)
         }
-        
+  
     }
 
     function increaseProjects(){
-        console.log(projectsEnd)
-        console.log(projects.length)
         if (projectsEnd !== projects.length){
             projectsContainer.innerHTML= ''
             projectsStart = projectsStart + 1
             projectsEnd = projectsEnd + 1
             projects.slice(projectsStart,projectsEnd).forEach(renderProject)
         }
-        
     }
     
     projects.slice(projectsStart,projectsEnd).forEach(renderProject)
@@ -80,10 +93,10 @@ document.addEventListener("DOMContentLoaded", () => {
         const meOrNotImg = document.getElementById("meOrNot")
         if (!meOrNot) {
             meOrNot = true
-            meOrNotImg.attributes.src.value = "images/me.png"
+            meOrNotImg.attributes.src.value = "images/me_sketch.png"
         } else {
             meOrNot = false
-            meOrNotImg.attributes.src.value = "images/me.svg"
+            meOrNotImg.attributes.src.value = "images/me.png"
         }
 
     }
@@ -128,31 +141,38 @@ document.addEventListener("DOMContentLoaded", () => {
 const projects = [
     {
         projectName : "Spot Check: Chicago",
-        links : "https://spotcheckchicago.onrender.com/",
+        links : [{Website: "https://spotcheckchicago.onrender.com/"}, {Walkthrough: "https://www.youtube.com/watch?v=QobCGprINfw&t=10s"}, {Repo: "https://github.com/jedwardmook/spot-check-app-clone"}],
         description : "Application that utilizes google-maps-api to allow users to add, favorite, and review skatespots usings their real lattitude and longitude.",
         technologysUsed : [" Javascript", " React", " Ruby on Rails", " Sass", " Google Maps Api"],
         image : "images/spot_check-2.png",
     },
     {
         projectName : "HeirBnb",
-        links : "https://www.youtube.com/watch?v=TOzLQ-lzTz8",
+        links : [{Walkthrough: "https://www.youtube.com/watch?v=TOzLQ-lzTz8"}, {Repo: "https://github.com/jedwardmook/phase-4-project"}],
         description : "HeirBnb is a full-stack project spoofing AirBnb through the lens of A Song of Ice and Fire by George RR Martin.",
         technologysUsed : [" Javascript", " React", " Ruby on Rails"],
         image : "images/logo.jpg",
     },
     {
         projectName : "4LW Notes",
-        links : "https://github.com/jedwardmook/phase-3-sinatra-react-project-frontend",
+        links : [{Walkthrough: "https://www.youtube.com/watch?v=--u0uSF--3I&t=25s"}, {Repo: "https://github.com/jedwardmook/phase-3-sinatra-react-project-frontend"}],
         description : "An application to keep daily notes at 4LW coffee shop. Employees can create and log notes of day to day activities at a coffee shop.",
         technologysUsed : [" Javascript", " React", " Sinatra"],
         image : "images/home.gif",
     },
     {
         projectName : "Spot Check",
-        links : "https://www.youtube.com/watch?v=hpDgLU6HpMg&t=1s",
+        links : [{Walkthrough: "https://www.youtube.com/watch?v=hpDgLU6HpMg&t=1s"}, {Repo: "https://github.com/jedwardmook/phase-2-project"}],
         description : "Application that utilizes google-maps-api to allow users to add, favorite, and review skatespots usings their real lattitude and longitude.",
         technologysUsed : [" Javascript", " React"],
         image : "images/spot_check.png",
+    },
+    {
+        projectName : "Simple Record Collector",
+        links : [{Walkthrough: "https://www.youtube.com/watch?v=v9wsrlNCvCo&t=1s"}, {Repo: "https://github.com/jedwardmook/phase-1-project"}],
+        description : "Simple Record Collector allows users to add albums to a virtual collection using the album's name, cover image, artist, genre, and year released. Users can search through their collection using the same attributes.",
+        technologysUsed : [" Vanilla Javascript", " HTML", " CSS"],
+        image : "images/add_form_gif.gif",
     }
 ]
 
