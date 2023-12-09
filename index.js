2feb73b73ff1704ace7cda3d4d6976f913c8c3be
JedwardMook <jedwardmook@yahoo.com>
2023-12-08 19:39:16 -0600

    sets up all the logic for the view more images. need to style and add some to ux of more photos

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const heroProjectViewer = document.createElement("div");
     heroProjectViewer.id = "none"
 
+
+
     const projectsContainer = document.getElementById('projects_container')
 
     const face = document.getElementById('face')
@@ -13,16 +15,61 @@ document.addEventListener("DOMContentLoaded", () => {
     let meOrNot = true
 
     function renderHeroProject(project){
-        const { projectName, links, description, technologysUsed, image} = project
+        const { projectName, links, description, technologysUsed, images} = project
+
         function heroProjectImageOnClick (){
-            heroProjectViewer.id = "hero_project_viewer";
+            heroProjectViewer.id = "hero_project_viewer"
             heroProjectViewer.innerHTML = `
-            <div id="viewer_image_div">
-                <img src=${image} alt=${projectName} id="hero_project_viewer_image" />
-            <div>
+            <div id="viewer_image_background">
+                <div><button id="close_button">&#10006</button></div>
+                <div id="viewer_image_main>
+                    <div id="viewer_image_div">
+                        ${displayLargeImage(images[0])}
+                    </div>
+                    <div id="hero_project_images" >
+                        ${displayAllImages(images)} 
+                    </div>
+                </div>
+            </div>
             `
+            heroProjectViewer.addEventListener('click', handleViewerContainerClick)
             heroContainer.append(heroProjectViewer)
+
+            function displayLargeImage(image){
+                const largeImageDiv = document.createElement("div")
+                largeImageDiv.id = "viewer_image_div"
+                largeImageDiv.innerHTML = `
+                    <img src=${image.link} alt=${image.imageDescription} id="hero_project_viewer_image" />
+                    <p id="hero_project_viewer_description">${image.imageDescription}</p>
+                `
+                return largeImageDiv.outerHTML;
+            }
+                
+            function handleViewerContainerClick(e){
+                if (e.target.id === 'close_button'){
+                    heroProjectViewer.remove()
+                }else if (e.target.id === "viewer_image_image"){
+                    const largeImageDiv = document.getElementById('viewer_image_div');
+                    largeImageDiv.innerHTML = displayLargeImage({
+                        link: e.target.src, 
+                        imageDescription: e.target.alt
+                    })
+                }
+            }
+
+            function displayAllImages(arr){
+                let imagesHTML = ''
+                arr.forEach(obj => {
+                    const img = document.createElement('img');
+                    img.id = "viewer_image_image";
+                    img.src = obj.link;
+                    img.alt = obj.imageDescription
+                    imagesHTML += img.outerHTML
+                })
+                return imagesHTML
+            }
         }
+
         const heroContainer = document.getElementById("hero_project_container")
         heroProject.innerHTML =`
             <div id="hero_project_info">
@@ -33,7 +80,7 @@ document.addEventListener("DOMContentLoaded", () => {
               <h4 id="hero_project_key">Technologies Used:</h4><p class="hero_project_value"> ${technologysUsed}<p>
             </div>
             <div>
-                <img src=${image} alt="${projectName}" id="hero_project_image"/>
+                <img src=${images[0].link} alt="${projectName}" id="hero_project_image"/>
             </div>`;
         heroContainer.append(heroProject);
         const linksContainer = document.getElementById('links')
@@ -56,7 +103,7 @@ document.addEventListener("DOMContentLoaded", () => {
     renderHeroProject(initial_hero_project)
 
     function renderProject(project){
-        const {image, projectName} = project
+        const {images, projectName} = project
         const projectCard = document.createElement("div")
         projectCard.id = "project_card";
         projectCard.addEventListener('click', () => {
@@ -64,7 +111,7 @@ document.addEventListener("DOMContentLoaded", () => {
             renderHeroProject(initial_hero_project)
         })
         projectCard.innerHTML = `
-            <img src=${image} alt="${projectName}" id="project_card_image" />
+            <img src=${images[0].link} alt="${projectName}" id="project_card_image" />
             <h4 id="project_card_name">${projectName}</h4>
         `;
         projectsContainer.append(projectCard);
@@ -157,35 +204,35 @@ const projects = [
         links : [{Website: "https://spotcheckchicago.onrender.com/"}, {Walkthrough: "https://www.youtube.com/watch?v=QobCGprINfw&t=10s"}, {Repo: "https://github.com/jedwardmook/spot-check-app-clone"}],
         description : "Application that utilizes google-maps-api to allow users to add, favorite, and review skatespots usings their real lattitude and longitude.",
         technologysUsed : [" Javascript", " React", " Ruby on Rails", " Sass", " Google Maps Api"],
-        image : "images/spot_check-2.png",
+        images : [{link: "images/spot_check-2.png", imageDescription: "Main logo for Spot Check - Chicago"}, {link: "images/logo.jpg", imageDescription: "Main logo for Spot Check - Chicago"}, {link: "images/home.gif", imageDescription: "Main logo for Spot Check - Chicago"}, {link: "images/spot_check-2.png", imageDescription: "Main logo for Spot Check - Chicago"}, {link: "images/spot_check-2.png", imageDescription: "Main logo for Spot Check - Chicago"}]
     },
     {
         projectName : "HeirBnb",
         links : [{Walkthrough: "https://www.youtube.com/watch?v=TOzLQ-lzTz8"}, {Repo: "https://github.com/jedwardmook/phase-4-project"}],
         description : "HeirBnb is a full-stack project spoofing AirBnb through the lens of A Song of Ice and Fire by George RR Martin.",
         technologysUsed : [" Javascript", " React", " Ruby on Rails"],
-        image : "images/logo.jpg",
+        images : [{link: "images/logo.jpg", imageDescription : "Main logo for HeirBnb"}, {link: "images/logo.jpg", imageDescription : "Main logo for HeirBnb"}, {link: "images/logo.jpg", imageDescription : "Main logo for HeirBnb"}, {link: "images/logo.jpg", imageDescription : "Main logo for HeirBnb"} ]
     },
     {
         projectName : "4LW Notes",
         links : [{Walkthrough: "https://www.youtube.com/watch?v=--u0uSF--3I&t=25s"}, {Repo: "https://github.com/jedwardmook/phase-3-sinatra-react-project-frontend"}],
         description : "An application to keep daily notes at 4LW coffee shop. Employees can create and log notes of day to day activities at a coffee shop.",
         technologysUsed : [" Javascript", " React", " Sinatra"],
-        image : "images/home.gif",
+        images : [{link: "images/home.gif", imageDescription : "A gif of the UX of 4LW Notes"}]
     },
     {
         projectName : "Spot Check",
         links : [{Walkthrough: "https://www.youtube.com/watch?v=hpDgLU6HpMg&t=1s"}, {Repo: "https://github.com/jedwardmook/phase-2-project"}],
         description : "Application that utilizes google-maps-api to allow users to add, favorite, and review skatespots usings their real lattitude and longitude.",
         technologysUsed : [" Javascript", " React"],
-        image : "images/spot_check.png",
+        images : [{link: "images/spot_check.png", imageDescription : "Main logo for Spot Check"}]
     },
     {
         projectName : "Simple Record Collector",
         links : [{Walkthrough: "https://www.youtube.com/watch?v=v9wsrlNCvCo&t=1s"}, {Repo: "https://github.com/jedwardmook/phase-1-project"}],
         description : "Simple Record Collector allows users to add albums to a virtual collection using the album's name, cover image, artist, genre, and year released. Users can search through their collection using the same attributes.",
         technologysUsed : [" Vanilla Javascript", " HTML", " CSS"],
-        image : "images/add_form_gif.gif",
+        images : [{link: "images/add_form_gif.gif", imageDescription : "A gif showing the UX of a Simple Record Collector"}]
     }
 ]
 
