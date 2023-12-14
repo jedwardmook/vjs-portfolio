1769ee16697e118f0e48ba9f403a990fbb47c859
JedwardMook <jedwardmook@yahoo.com>
2023-12-13 19:03:06 -0600

    A lot of images added and added to mian object

--- index.js
+++ index.js
@@ -150,10 +150,10 @@ document.addEventListener("DOMContentLoaded", () => {
         const meOrNotImg = document.getElementById("meOrNot")
         if (!meOrNot) {
             meOrNot = true
-            meOrNotImg.attributes.src.value = "images/me_sketch.png"
+            meOrNotImg.attributes.src.value = "images/me.png"
         } else {
             meOrNot = false
-            meOrNotImg.attributes.src.value = "images/me.png"
+            meOrNotImg.attributes.src.value = "images/me_sketch.png"
         }
     }
 
@@ -217,28 +217,46 @@ const projects = [
         links : [{Walkthrough: "https://www.youtube.com/watch?v=TOzLQ-lzTz8"}, {Repo: "https://github.com/jedwardmook/phase-4-project"}],
         description : "HeirBnb is a full-stack project spoofing AirBnb through the lens of A Song of Ice and Fire by George RR Martin.",
         technologysUsed : [" Javascript", " React", " Ruby on Rails"],
-        images : [{link: "images/logo.jpg", imageDescription : "Main logo for HeirBnb"}, {link: "images/logo.jpg", imageDescription : "Main logo for HeirBnb"}, {link: "images/logo.jpg", imageDescription : "Main logo for HeirBnb"}, {link: "images/logo.jpg", imageDescription : "Main logo for HeirBnb"} ]
+        images : [
+            {link: "images/HBNB-main.jpg", imageDescription : "Collage of components for HeirBnb"}, 
+            {link: "images/HBNB-home.jpg", imageDescription : "Home pages featuring for cards for castle locations"}, 
+            {link: "images/HBNB-location.jpg", imageDescription : "A location's page featuring its amenities and reviews"}, 
+            {link: "images/HBNB-profile.jpg", imageDescription : "A user's profile page"}, 
+            {link: "images/HBNB-editprofile.jpg", imageDescription : "UI for editing a user's profile page"}
+        ]
     },
     {
         projectName : "4LW Notes",
         links : [{Walkthrough: "https://www.youtube.com/watch?v=--u0uSF--3I&t=25s"}, {Repo: "https://github.com/jedwardmook/phase-3-sinatra-react-project-frontend"}],
         description : "An application to keep daily notes at 4LW coffee shop. Employees can create and log notes of day to day activities at a coffee shop.",
         technologysUsed : [" Javascript", " React", " Sinatra"],
-        images : [{link: "images/home.gif", imageDescription : "A gif of the UX of 4LW Notes"}]
+        images : [
+            {link: "images/4LW-main.jpg", imageDescription : "Collage of components for 4LW Notes"},
+            {link: "images/4LW-home.jpg", imageDescription : "Home page showing most recent notes and filtering abilities"},
+            {link: "images/4LW-note.jpg", imageDescription : "Form for adding a not to the database"},
+            {link: "images/4LW-employee.jpg", imageDescription : "Form for adding an employee to the database"},
+        ]
     },
     {
         projectName : "Spot Check",
         links : [{Walkthrough: "https://www.youtube.com/watch?v=hpDgLU6HpMg&t=1s"}, {Repo: "https://github.com/jedwardmook/phase-2-project"}],
         description : "Application that utilizes google-maps-api to allow users to add, favorite, and review skatespots usings their real lattitude and longitude.",
         technologysUsed : [" Javascript", " React"],
-        images : [{link: "images/spot_check.png", imageDescription : "Main logo for Spot Check"}]
+        images : [{link: "images/SC-main.jpg", imageDescription : "Home page and logo"},
+                {link: "images/SC-home.jpg", imageDescription : "Home page showing spots and filter options"},
+                {link: "images/SC-spot.jpg", imageDescription : "Detailed view of a spot's page"},
+                {link: "images/SC-add.jpg", imageDescription : "Add form for adding a spot"},
+        ]
     },
     {
         projectName : "Simple Record Collector",
         links : [{Walkthrough: "https://www.youtube.com/watch?v=v9wsrlNCvCo&t=1s"}, {Repo: "https://github.com/jedwardmook/phase-1-project"}],
         description : "Simple Record Collector allows users to add albums to a virtual collection using the album's name, cover image, artist, genre, and year released. Users can search through their collection using the same attributes.",
         technologysUsed : [" Vanilla Javascript", " HTML", " CSS"],
-        images : [{link: "images/add_form_gif.gif", imageDescription : "A gif showing the UX of a Simple Record Collector"}]
+        images : [{link: "images/SRC-home.jpg", imageDescription : "Home page showing record collection"},
+                {link: "images/SRC-add.jpg", imageDescription : "Home page featuring the add album component"},
+                {link: "images/SRC-search.jpg", imageDescription : "Home page featuring the search component"},
+        ]
     }
 ]
 
